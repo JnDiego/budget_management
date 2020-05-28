d70567c35ce37ce80d533df6a7b4180ab1ef039d
Juan Diego <diego_suarez@outlook.com>
2020-05-28 12:44:07 -0500

    Updates

---
+++ precache-manifest.e1c4420f762c549ec8ecdf258abba728.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2516dc8095b4c9d49aae0b6e15f16535",
+    "url": "/budget_management/index.html"
+  },
+  {
+    "revision": "2b993f12024d4982fc00",
+    "url": "/budget_management/static/css/main.f5cd12b7.chunk.css"
+  },
+  {
+    "revision": "2b7c1841568d80efaf33",
+    "url": "/budget_management/static/js/2.8ba7cf60.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/budget_management/static/js/2.8ba7cf60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b993f12024d4982fc00",
+    "url": "/budget_management/static/js/main.9be03d76.chunk.js"
+  },
+  {
+    "revision": "690fc3f3815d3c46a470",
+    "url": "/budget_management/static/js/runtime-main.454c8547.js"
+  }
+]);
